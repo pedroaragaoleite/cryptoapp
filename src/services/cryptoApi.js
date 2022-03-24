c5d8c863ca1974b9fd9ec72a74bd9743c80d3723
pedroaragaoleite <33551952+pedroaragaoleite@users.noreply.github.com>
2022-03-24 15:20:16 +0100

    fetch crypto details data
    crypto details data id working

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '9b25986538mshc3a95a2899308acp1e424cjsn3e98d3433bf2'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptoDetails: builder.query({
+            query: (id) => createRequest(`/coin/${id}`),
+        })
+    })
+});
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
+
