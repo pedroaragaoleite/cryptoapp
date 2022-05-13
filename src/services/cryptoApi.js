5b060a5c5e50aa263a123432399961f1162e7834
Pedro Aragão <pedro.aragao.leite@gmail.com>
2022-05-13 15:33:16 +0200

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '92698b3961msh8ffa7a482fcc0f3p1b270bjsn447eb87bfdc9'
+    'X-RapidAPI-Key': '92698b3961msh8ffa7a482fcc0f3p1b270bjsn447eb87bfdc9';
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
